25d61865b001b84abc951b1799782bc6e73ec34f
sherinaayu <sherinaayuwp07@gmail.com>
2024-03-07 13:23:09 +0700

    js 4 praktikum 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,18 +9,18 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        //$data =[
-         //   'username'=>'customer-1',
-          //  'nama'=>'Pelanggan',
-           // 'password'=>Hash::make('12345'),
-          //  'level_id'=>4
-       // ];
-       // UserModel::insert($data);
+        $data =[
+            'level_id'=>2,
+            'username'=>'manager_tiga',
+            'nama'=>'Manager 3',
+            'password'=>Hash::make('12345')
+        ];
+        UserModel::insert($data);
         //tambah data user dengan Eloquent Model
-        $data = [
-           'nama' => 'Pelanggan Pertama',
-       ];
-        UserModel::where('username', 'customer-1')->update($data); //tambahkan data ke tabel m_user
+        //$data = [
+           //'nama' => 'Pelanggan Pertama',
+       //];
+       // UserModel::where('username', 'customer-1')->update($data); //tambahkan data ke tabel m_user
 
         //coba akses model UserModel
         $user = UserModel::all(); //ambil semua data dari tabel m_user
