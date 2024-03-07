946828323e56ec7bbb14a257cac6165ddb1e5768
sherinaayu <sherinaayuwp07@gmail.com>
2024-03-07 14:52:59 +0700

    js 4 praktikum 2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,15 +23,38 @@ class UserController extends Controller
        // UserModel::where('username', 'customer-1')->update($data); //tambahkan data ke tabel m_user
 
         //coba akses model UserModel
-        $user = UserModel::firstOrNew(
+        $user = UserModel::create(
             [
-                'username'=>'manager33',
-                'nama'=>'Manager Tiga Tiga',
+                'username'=>'manager11',
+                'nama'=>'Manager11',
                 'password'=>Hash::make('12345'),
                 'level_id'=>2
             ],
         ); 
+        $user->username='manager12';
         $user->save();
+        $user->wasChanged();//true
+        $user->wasChanged('username');//true
+        $user->wasChanged(['username','level_id']);//true
+        $user->wasChanged('nama');//false
+        dd($user->wasChanged(['nama','username']));//true
+
+        //$user->isDirty();//true
+        //$user->isDirty('username');//true
+        //$user->isDirty('nama');//false
+        //$user->isDirty(['nama','username']);//true
+
+        //$user->isClean();//false
+        //$user->isClean('username');//false
+        //$user->isClean('nama');//true
+        //$user->isClean(['nama','username']);//false
+
+
+        //$user->isDirty();//false
+        //$user->isClean();//true
+        //dd($user->isDirty());
+
+        
         //$user = UserModel::where('level_id',2)->count(); 
         //dd($user);
         return view('user', ['data' => $user]);
