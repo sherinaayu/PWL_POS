69284fcb6fb1f85cda37c08d43652215abfa42ef
sherinaayu <sherinaayuwp07@gmail.com>
2024-05-07 19:51:45 +0700

    Jobsheet 11 Tugas tabel t_penjualan_detail

---
+++ app/Models/PenjualanModel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class PenjualanModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 't_penjualan';
+    protected $primaryKey = 'penjualan_id';
+    /**
+     * The attributes that are mass assignable.
+     * 
+     * @var array
+     */
+    protected $fillable = ['user_id', 'pembeli', 'penjualan_kode', 'penjualan_tanggal'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(UserModel::class, 'user_id', 'user_id');
+    }
+}
--- app/Models/TransaksiModel.php
+++ app/Models/TransaksiModel.php
@@ -5,22 +5,35 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class TransaksiModel extends Model
 {
     use HasFactory;
 
-    protected $table = 't_penjualan';
-    protected $primaryKey = 'penjualan_id';
+    protected $table = 't_penjualan_detail';
+    protected $primaryKey = 'detail_id';
     /**
      * The attributes that are mass assignable.
      * 
      * @var array
      */
-    protected $fillable = ['user_id', 'pembeli', 'penjualan_kode', 'penjualan_tanggal'];
+    protected $fillable = ['penjualan_id', 'barang_id', 'harga', 'jumlah'];
 
-    public function user(): BelongsTo
+    public function penjualan(): BelongsTo
     {
-        return $this->belongsTo(UserModel::class, 'user_id', 'user_id');
+        return $this->belongsTo(PenjualanModel::class, 'penjualan_id', 'penjualan_id');
+    }
+
+    public function barang(): BelongsTo
+    {
+        return $this->belongsTo(BarangModel::class, 'barang_id', 'barang_id');
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' . $image),
+        );
     }
 }
